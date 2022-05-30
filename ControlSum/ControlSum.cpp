e01fcfd55fcdf9dd2a9524cc582d4f66b2961bc3
Daniel <danya.ostap@gmail.com>
2022-05-30 12:40:13 +0300

    added crc32 function

--- ControlSum/ControlSum.cpp
+++ ControlSum/ControlSum.cpp
@@ -1,20 +1,31 @@
-// ControlSum.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <tchar.h>
+
+uint64_t crc32(char* buf, uint64_t len)
+{
+    uint64_t crc_table[256];
+    uint64_t crc;
+
+    for (int i = 0; i < 256; i++)
+    {
+        crc = i;
+        for (int j = 0; j < 8; j++)
+            crc = crc & 1 ? (crc >> 1) ^ 0xEDB88320UL : crc >> 1;
+
+        crc_table[i] = crc;
+    };
+
+    crc = 0xFFFFFFFFUL;
+
+    while (len--)
+        crc = crc_table[(crc ^ *buf++) & 0xFF] ^ (crc >> 8);
+
+    return crc ^ 0xFFFFFFFFUL;
+};
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
