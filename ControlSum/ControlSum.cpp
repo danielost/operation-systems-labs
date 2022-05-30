225d912df860eed3674eab1fd0ace87c8530be10
Daniel <danya.ostap@gmail.com>
2022-05-30 12:42:58 +0300

    added a function to write the control sum to DLLs

--- ControlSum/ControlSum.cpp
+++ ControlSum/ControlSum.cpp
@@ -25,6 +25,28 @@ uint64_t crc32(char* buf, uint64_t len)
     return crc ^ 0xFFFFFFFFUL;
 };
 
+void WriteSum(char* path) {
+    std::fstream file;
+    file.open(path, std::ios::out | std::ios::in | std::ios::ate | std::ios::binary);
+    if (!file)
+        std::cout << "Error!";
+    else {
+        std::streamoff size = file.tellg();
+        file.seekg(0, std::ios::beg);
+        char* x = new char[size];
+        int sz = size;
+        memset(x, 0, sizeof(x) + 1);
+
+        file.read(x, size);
+
+        uint64_t crc = crc32(x, size);
+
+        file.seekg(0, std::ios::end);
+        file.write((char*)&crc, sizeof(crc));
+        file.close();
+    }
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
