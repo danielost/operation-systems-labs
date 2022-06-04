f175749b875624c89e0799abfbd850445808cca1
1KitCat1 <denischool119@gmail.com>
2022-06-04 19:25:30 +0300

    Created Mail struct

--- lab4/MailboxLab4/MailboxLab4/MailboxLab4.cpp
+++ lab4/MailboxLab4/MailboxLab4/MailboxLab4.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <windows.h>
+#include <vector>
+#include <tchar.h>
+
+struct Mail {
+	size_t msgCount = 0;
+	LPCWSTR path;
+	HANDLE handler;
+	DWORD maxSize;
+	
+	Mail(LPCWSTR path, int maxSize) {
+		this->path = path;
+		this->maxSize = maxSize;
+	}
+};
 
 int main() {
-
+	Mail mail(_T("../"), 32);
 }
